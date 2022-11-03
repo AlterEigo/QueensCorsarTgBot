2b5d8ae4c3d4a3f68f86f779267b46c369618944
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-03 16:53:01 +0300

    Add: fetching api token and printing some infos

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,27 @@ mod logger;
 mod core;
 mod prelude;
 
-fn main() {
-    println!("Hello, world!");
+use crate::prelude::*;
+
+const CRATE_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn main() -> UResult {
+    let logger = configure_term_root();
+
+    info!(logger, "Starting QueenCorsar telegram bot";
+        "upstream" => "https://github.com/AlterEigo/QueensCorsarTgBot",
+        "email" => "iaroslav.sorokin@gmail.com",
+        "author" => "Iaroslav Sorokin",
+        "version" => CRATE_VERSION,
+    );
+
+    let token = std::env::var("QUEENSCORSAR_TG_TOKEN");
+    if token.is_err() {
+        crit!(logger, "Could not fetch API token from the environment");
+        return Err(token.err().unwrap().into());
+    }
+    let token = token.unwrap();
+    debug!(logger, "API token fetched");
+
+    Ok(())
 }
