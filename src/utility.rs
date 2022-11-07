7e5e23edec6c5ebc535aecbac7ed6ace980dc132
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-07 16:42:40 +0300

    Add: reading certificates and the private key

--- src/utility.rs
+++ src/utility.rs
@@ -1,9 +1,32 @@
+use std::io::BufReader;
+use std::iter;
+
 use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{Item, read_one};
 
 use crate::prelude::UResult;
 
 pub fn load_x509_credentials() -> UResult<(Vec<Certificate>, PrivateKey)> {
-    todo!()
+    let cert_file = std::fs::File::open(std::path::Path::new("tgbot.crt"))?;
+    let key_file = std::fs::File::open(std::path::Path::new("tgbot.key"))?;
+    let mut cert_reader = BufReader::new(cert_file);
+    let mut key_reader = BufReader::new(key_file);
+
+    let mut certs = Vec::new();
+    for item in iter::from_fn(|| read_one(&mut cert_reader).transpose()) {
+        match item.unwrap() {
+            Item::X509Certificate(cert) => certs.push(Certificate(cert)),
+            _ => return Err("Expected X509 certificates, found something else".into())
+        }
+    }
+    let key = {
+        let item = read_one(&mut key_reader)?.unwrap();
+        match item {
+            Item::RSAKey(key) => PrivateKey(key),
+            _ => return Err("Expected an RSA private key, found something else".into())
+        }
+    };
+    Ok((certs, key))
 }
 
 pub fn create_server_config() -> UResult<ServerConfig> {
