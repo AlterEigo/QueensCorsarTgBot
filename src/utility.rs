74ae76e225e572cd4176d4b7cc231f00e2d8e296
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-08 12:10:52 +0300

    Mod: splitted file reading into several functions

--- src/utility.rs
+++ src/utility.rs
@@ -6,11 +6,9 @@ use rustls_pemfile::{read_one, Item};
 
 use crate::prelude::UResult;
 
-pub fn load_x509_credentials() -> UResult<(Vec<Certificate>, PrivateKey)> {
-    let cert_file = std::fs::File::open(std::path::Path::new("tgbot.crt"))?;
-    let key_file = std::fs::File::open(std::path::Path::new("tgbot.key"))?;
+pub fn load_x509_certs(crt_path: &str) -> UResult<Vec<Certificate>> {
+    let cert_file = std::fs::File::open(std::path::Path::new(crt_path))?;
     let mut cert_reader = BufReader::new(cert_file);
-    let mut key_reader = BufReader::new(key_file);
 
     let mut certs = Vec::new();
     for item in iter::from_fn(|| read_one(&mut cert_reader).transpose()) {
@@ -19,6 +17,13 @@ pub fn load_x509_credentials() -> UResult<(Vec<Certificate>, PrivateKey)> {
             _ => return Err("Expected X509 certificates, found something else".into()),
         }
     }
+    Ok(certs)
+}
+
+pub fn load_x509_secret_key(key_path: &str) -> UResult<PrivateKey> {
+    let key_file = std::fs::File::open(std::path::Path::new(key_path))?;
+    let mut key_reader = BufReader::new(key_file);
+
     let key = {
         let item = read_one(&mut key_reader)?.unwrap();
         match item {
@@ -26,6 +31,12 @@ pub fn load_x509_credentials() -> UResult<(Vec<Certificate>, PrivateKey)> {
             _ => return Err("Expected an RSA private key, found something else".into()),
         }
     };
+    Ok(key)
+}
+
+pub fn load_x509_credentials() -> UResult<(Vec<Certificate>, PrivateKey)> {
+    let certs = load_x509_certs("tgbot.crt")?;
+    let key = load_x509_secret_key("tgbot.key")?;
     Ok((certs, key))
 }
 
