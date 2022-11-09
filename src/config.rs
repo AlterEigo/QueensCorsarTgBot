2200ea1ddd6cf4225a8f6b5653658767a27cb309
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-09 15:11:45 +0300

    Add: now reading project settings from the config file

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,54 @@
-use std::path::Path;
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+use std::io::Write;
+use std::io::{BufRead, BufReader};
 
-const SERVER_IP: &str = "";
-const SERVER_PORT: u16 = 8443;
-const SERVER_KEY_PATH: &str = "tgbot.key";
-const SERVER_CERT_PATH: &str = "tgbot.cert";
-const PACKAGE_VERSION: &str = std::env!("CARGO_PKG_VERSION");
-const TOKEN_VARNAME: &str = "QUEENSCORSAR_TG_TOKEN";
+#[derive(Deserialize, Serialize)]
+pub struct Config {
+    pub server_ip: String,
+    pub server_port: u16,
+    pub private_key_path: String,
+    pub certificate_path: String,
+    pub token_var: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        toml::from_str::<Config>(
+            r#"
+            [config]
+            server_ip = '127.0.0.1'
+            server_port = 8443
+            private_key_path = 'private.key'
+            certificate_path = 'server.crt'
+            token_var = 'QUEENSCORSAR_TG_TOKEN'
+            "#,
+        )
+        .unwrap()
+    }
+}
+
+pub const PACKAGE_VERSION: &'static str = std::env!("CARGO_PKG_VERSION");
+
+lazy_static! {
+    pub static ref APP_CONFIG: Config = {
+        const FILENAME: &'static str = "bot_config.toml";
+        let file = match std::fs::File::open(FILENAME) {
+            Ok(opened) => opened,
+            Err(why) => {
+                if why.kind() == std::io::ErrorKind::NotFound {
+                    let default_config = Config::default();
+                    let default_config = toml::to_string(&default_config).unwrap();
+                    let mut file = std::fs::File::create(FILENAME).unwrap();
+                    write!(file, "{}", default_config).unwrap();
+                    file
+                } else {
+                    panic!("{:#?}", why);
+                }
+            }
+        };
+        let mut reader = BufReader::new(file);
+        let contents = String::from_utf8(reader.fill_buf().unwrap().to_vec()).unwrap();
+        toml::from_str::<Config>(&contents).unwrap()
+    };
+}
