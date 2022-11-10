1a8565e4e62186769766116b7b786c00299c9fdf
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-10 15:28:59 +0300

    Add: importing qcproto items within the prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,4 +4,5 @@ pub type UResult<T = ()> = Result<T, Box<dyn std::error::Error + Send + Sync>>;
 pub use crate::core::*;
 pub use crate::logger::*;
 pub use crate::utility::*;
+pub use crate::qcproto::*;
 pub use slog::{crit, debug, error, info, o, warn};
