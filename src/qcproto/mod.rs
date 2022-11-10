d6929dbb78ca04b71f7e86add68d15c42ce1c538
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-10 14:53:20 +0300

    Mod: command provider builder

--- src/qcproto/mod.rs
+++ src/qcproto/mod.rs
@@ -1,7 +1,9 @@
 use std::fmt::Display;
+use std::os::unix::net::UnixListener;
 use std::time::Duration;
 use std::{os::unix::net::UnixStream, io::Read};
 use std::io::Write;
+use std::sync::atomic::AtomicBool;
 
 use serde::{Serialize,Deserialize};
 use slog::Logger;
@@ -31,7 +33,54 @@ impl Display for TransmissionResult {
     }
 }
 
-pub struct CommandProvider;
+pub struct CommandProvider {
+    tunnel: UnixListener,
+    stop_requested: AtomicBool,
+    logger: Logger,
+    handler: Box<dyn CommandHandler>,
+}
+
+#[derive(Default)]
+pub struct CommandProviderBuilder {
+    unix_listener: Option<UnixListener>,
+    logger: Option<Logger>,
+    command_handler: Option<Box<dyn CommandHandler>>,
+}
+
+impl CommandProviderBuilder {
+    pub fn listener(self, listener: UnixListener) -> Self {
+        Self {
+            unix_listener: Some(listener),
+            ..self
+        }
+    }
+
+    pub fn logger(self, logger: Logger) -> Self {
+        Self {
+            logger: Some(logger),
+            ..self
+        }
+    }
+
+    pub fn command_handler(self, handler: Box<dyn CommandHandler>) -> Self {
+        Self {
+            command_handler: Some(handler),
+            ..self
+        }
+    }
+
+    pub fn build(self) -> UResult<CommandProvider> {
+        let provider = CommandProvider {
+            tunnel: self
+                .unix_listener
+                .ok_or("Unix socket listener not provided".to_owned())?,
+            stop_requested: false.into(),
+            logger: self.logger.ok_or("Logger not provided".to_owned())?,
+            handler: self.command_handler.ok_or("Command handler not provided".to_owned())?,
+        };
+        Ok(provider)
+    }
+}
 
 pub struct CommandSender {
     tunnel: UnixStream,
