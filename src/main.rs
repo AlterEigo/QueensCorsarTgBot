24423297f1c0f24b1b0e703816bcf8a650fc5f7f
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-11 14:11:17 +0300

    Fix: used the wrong config name

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ use crate::prelude::*;
 #[tokio::main]
 async fn main() -> UResult {
     let requirements = application::BootstrapRequirements {
-        logger: logger::configure_compact_root()?,
-        config: config::read_or_create("tgbot.toml")?,
+        logger: logger::configure_term_root(),
+        config: config::read_or_create("bot_config.toml")?,
     };
 
     application::bootstrap(requirements).await
