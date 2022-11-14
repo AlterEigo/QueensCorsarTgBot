57bfbb7e8edd15822dccccad92d76a0dc6961c20
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-14 11:22:59 +0300

    Add: basic dispatcher interface

--- src/core/dispatcher.rs
+++ src/core/dispatcher.rs
@@ -1,5 +1,22 @@
+use telegram_bot_api::types::{Update, Message};
+
 use crate::prelude::*;
 
 pub trait Dispatcher<T> {
     fn dispatch(&self, data: T) -> UResult;
+}
+
+pub struct UpdateDispatcher;
+pub struct CommandDispatcher;
+
+impl UpdateDispatcher {
+    pub fn message(&self, _msg: Message) -> UResult {
+        todo!()
+    }
+}
+
+impl Dispatcher<Update> for UpdateDispatcher {
+    fn dispatch(&self, _data: Update) -> UResult {
+        todo!()
+    }
 }
