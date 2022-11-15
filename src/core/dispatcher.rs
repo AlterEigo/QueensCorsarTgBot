0d9b73a413e1eebe1d937ab2a8166d44989154b2
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-15 12:41:27 +0300

    Add: now able to instantiate an update dispatcher

--- src/core/dispatcher.rs
+++ src/core/dispatcher.rs
@@ -14,6 +14,16 @@ pub struct CommandDispatcher {
     handler: Box<dyn CommandHandler>,
 }
 
+impl UpdateDispatcher {
+    fn new<T>(handler: T) -> Self
+        where for<'a> T: 'a + UpdateHandler
+    {
+        Self {
+            handler: Box::new(handler)
+        }
+    }
+}
+
 impl Dispatcher<Update> for UpdateDispatcher {
     fn dispatch(&self, _data: Update) -> UResult {
         todo!()
